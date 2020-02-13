aaf118970ef21a04051276cb8ada222b5b323396
Yiwei Liu (student) <2431251L@student.gla.ac.uk>
2020-02-13 17:38:08 +0000

    C6

--- populate_rango.py
+++ populate_rango.py
@@ -1,6 +1,5 @@
 import os
-os.environ.setdefault('DJANGO_SETTINGS_MODULE',
-                      'tango_with_django_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE','tango_with_django_project.settings')
 
 import django
 django.setup()
@@ -14,69 +13,69 @@ def populate():
     # through each data structure, and add the data to our models.
 
     python_pages = [
-        {'title': 'Official Python Tutorial',
-         'url': 'http://docs.python.org/3/tutorial/'},
-        {'title': 'How to Think like a Computer Scientist',
-         'url': 'http://www.greenteapress.com/thinkpython/'},
-        {'title': 'Learn Python in 10 Minutes',
-         'url': 'http://www.korokithakis.net/tutorials/python/'}]
+        {'title':'Official Python Tutorial',
+         'url':'http://docs.python.org/3/tutorial/',
+         'views':1024},
+        {'title':'How to Think like a Computer Scientist',
+         'url':'http://www.greenteapress.com/thinkpython/',
+         'views':2048},
+        {'title':'Learn Python in 10 Minutes',
+         'url':'http://www.korokithakis.net/tutorials/python/',
+         'views':4096}
+        ]
 
     django_pages = [
-        {'title': 'Official Django Tutorial',
-         'url': 'https://docs.djangoproject.com/en/2.1/intro/tutorial01/'},
-        {'title': 'Django Rocks',
-         'url': 'http://www.djangorocks.com/'},
-        {'title': 'How to Tango with Django',
-         'url': 'http://www.tangowithdjango.com/'}]
+        {'title':'Official Django Tutorial',
+         'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/',
+         'views':8192},
+        {'title':'Django Rocks',
+         'url':'http://www.djangorocks.com/',
+         'views':16384},
+        {'title':'How to Tango with Django',
+         'url':'http://www.tangowithdjango.com/',
+         'views':32768}
+        ]
 
     other_pages = [
-        {'title': 'Bottle',
-         'url': 'http://bottlepy.org/docs/dev/'},
-        {'title': 'Flask',
-         'url': 'http://flask.pocoo.org'}]
+        {'title':'Bottle',
+         'url':'http://bottlepy.org/docs/dev/',
+         'views':65536},
+        {'title':'Flask',
+         'url':'http://flask.pocoo.org',
+         'views':131072}
+        ]
 
-    cats = {'Python': {'pages': python_pages},
-            'Django': {'pages': django_pages},
-            'Other Frameworks': {'pages': other_pages}}
+    cats = {'Python': {'pages': python_pages,"views":128,"likes":64},
+            'Django': {'pages': django_pages,"views":64,"likes":32},
+            'Other Frameworks': {'pages': other_pages,"views":32,"likes":16}
+        }
 
     # If you want to add more categories or pages,
     # add them to the dictionaries above.
-
     # The code below goes through the cats dictionary, then adds each category,
     # and then adds all the associated pages for that category.
     for cat, cat_data in cats.items():
-        c = add_cat(cat)
+        c = add_cat(cat,cat_data["views"],cat_data["likes"])
         for p in cat_data['pages']:
-            add_page(c, p['title'], p['url'])
+            add_page(c, p['title'], p['url'],p["views"])
 
     # Print out the categories we have added.
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
             print(f'- {c}: {p}')
 
-def add_page(cat, title, url, views=0):
+def add_page(cat, title, url, views):
     p = Page.objects.get_or_create(category=cat, title=title)[0]
-    p.url = url
-    p.views = views
+    p.url=url
+    p.views=views
     p.save()
     return p
 
-def add_cat(name, views=0, likes=0):
-    c = Category.objects.get_or_create(name=name, views=views,likes=likes)[0]
-
-    if c.name == 'Python':
-        c.views, c.likes = 128, 64
-    elif c.name =='Django':
-        c.views, c.likes = 64, 32
-    elif c.name == 'Other Frameworks':
-        c.views, c.likes = 32, 16
-
+def add_cat(name,views,likes):
+    c = Category.objects.get_or_create(name=name)[0]
+    c.views = views
+    c.likes = likes
     c.save()
-
-    # Category.objects.filter(name='Python').update(views=128,likes=64)
-    # Category.objects.filter(name='Django').update(views=64,likes=32)
-    # Category.objects.filter(name='Other Frameworks').update(views=32,likes=16)
-
     return c
 
 # Start execution here!
